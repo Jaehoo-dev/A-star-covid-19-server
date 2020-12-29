1c7b5956da93307ee89cb7c3ae3b006f5c5a3145
Jaehoo Im <im_jaehoo@kakao.com>
2020-12-29 22:04:20 +0900

    feat: randomly generate one to three danger zones

--- src/routes/controllers/danger.controller.ts
+++ src/routes/controllers/danger.controller.ts
@@ -3,10 +3,12 @@ import generateDangerLocations from '../../utils/generateDangerLocation';
 import { NUMBER_OF_ROWS, NUMBER_OF_COLUMNS } from '../../constants/numbers';
 
 export const getDangerLocations = (req: Request, res: Response, next: NextFunction) => {
-  const dangerLocations = [
-    generateDangerLocations(NUMBER_OF_ROWS * NUMBER_OF_COLUMNS),
-    generateDangerLocations(NUMBER_OF_ROWS * NUMBER_OF_COLUMNS),
-  ];
+  const dangerLocations: number[] = [];
+  const numberOfDangerLocations = Math.floor(Math.random() * 3) + 1;
+
+  for (let i = 0; i < numberOfDangerLocations; i++) {
+    dangerLocations.push(generateDangerLocations(NUMBER_OF_ROWS * NUMBER_OF_COLUMNS));
+  }
 
   res.status(200).json({
     result: 'ok',
