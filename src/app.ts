401dd7fd7c7630c342cef0dc05b8c8e3c89914fa
Jaehoo Im <im_jaehoo@kakao.com>
2020-12-29 18:21:34 +0900

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express, { Application, Request, Response, NextFunction, Errback } from 'express';
+import createError from 'http-errors';
+import logger from 'morgan';
+import cors from 'cors';
+import http from 'http';
+import indexRouter from './routes/index';
+import Error from './interfaces/Error';
+
+const port: string | number = process.env.PORT || 8080;
+const app: Application = express();
+const server: http.Server = http.createServer(app);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/', indexRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404, 'not found'));
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  const message
+    = err.statusCode === 404
+      ? err.message
+      : 'internal server error';
+
+  res
+    .status(err.statusCode || 500)
+    .json({
+      result: 'failure',
+      message,
+    });
+});
+
+server.listen(port);
+
+export default app;
