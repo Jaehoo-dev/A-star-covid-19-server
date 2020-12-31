691036b1a022420872f640339f334f76b2c395aa
Jaehoo Im <im_jaehoo@kakao.com>
2021-01-01 02:26:03 +0900

    chore: migrate database to cloud

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const env = process.env;
 
 const sequelize = new Sequelize(env.POSTGRES_DATABASE!, env.POSTGRES_USERNAME!, env.POSTGRES_PASSWORD, {
-  host: 'localhost',
+  host: '34.64.255.176',
   dialect: 'postgres',
   pool: {
     max: 5,
@@ -12,5 +12,9 @@ const sequelize = new Sequelize(env.POSTGRES_DATABASE!, env.POSTGRES_USERNAME!, 
     idle: 10000,
   },
 });
+
+sequelize.authenticate()
+  .then(() => console.log('database connected'))
+  .catch(err => console.error(err));
 
 export default sequelize;
