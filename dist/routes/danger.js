3538706a9a2e22882e6a6d14e551f7816a35155a
Jaehoo Im <im_jaehoo@kakao.com>
2021-01-07 19:36:20 +0900

    deploy: remove dist and change scripts

--- dist/routes/danger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const danger_controller_1 = require("./controllers/danger.controller");
-const router = express_1.Router();
-router.get('/', danger_controller_1.getDangerLocations);
-exports.default = router;
--- dist/utils/generateDangerLocation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generateDangerLocation(max) {
-    return Math.floor(Math.random() * max);
-}
-exports.default = generateDangerLocation;
